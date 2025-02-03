ae6009d3cce429facbb045f7de7ee179e1c8f279
ValpsZ <limelgren@gmail.com>
2025-02-03 22:45:53 +0100

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::env;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use rusqlite::Connection;
+
+mod utils;
+
+async fn create_user(app_mode: web::Data<String>) -> impl Responder {
+    let conn = Connection::open("database.db");
+
+    match conn {
+        Ok(conn) => HttpResponse::Ok().json("a"),
+        Err(_) => utils::server_error("Error while connecting to database", app_mode),
+    }
+}
+
+async fn get_user(app_mode: web::Data<String>) -> impl Responder {
+    //    let conn: Result<Connection, rusqlite::Error> = Connection::open("database.db");
+    let conn: Result<Connection, rusqlite::Error> = Err(rusqlite::Error::ExecuteReturnedResults);
+
+    match conn {
+        Ok(conn) => HttpResponse::Ok().json("a"),
+        Err(_) => utils::server_error("Error while connecting to database", app_mode),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    /*
+    let conn = Connection::open("database.db")?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            hashed_password TEXT NOT NULL,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS accounts (
+            account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            balance DECIMAL(15, 2),
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+        )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS transactions (
+            transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sender_account_id INTEGER NOT NULL,
+            receiver_account_id INTEGER NOT NULL,
+            message TEXT,
+            amount DECIMAL(15, 2) NOT NULL,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (sender_account_id) REFERENCES accounts(sender_account_id) ON DELETE CASCADE,
+            FOREIGN KEY (receiver_account_id) REFERENCES accounts(receiver_account_id) ON DELETE CASCADE
+        )",
+        [],
+    )?;
+    */
+
+    // Load environment variables from .env in development mode
+    dotenv().ok();
+
+    let app_mode = env::var("APP_MODE").unwrap_or_else(|_| "prod".to_string());
+    let port = "127.0.0.1:2000";
+
+    match app_mode.to_string().as_str() {
+        "dev" => println!("Dev running on port {}", port),
+        "prod" => println!("Prod running on port {}", port),
+        _ => (),
+    }
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(app_mode.clone())) // Pass app_mode as shared data
+            .route("/create_user", web::post().to(create_user))
+            .route("/get_user", web::get().to(get_user))
+    })
+    .bind("127.0.0.1:2000")?
+    .run()
+    .await
+}
---
+++ src/utils.rs
@@ -0,0 +1,10 @@
+use actix_web::{body::BoxBody, web, HttpResponse};
+
+pub(crate) fn server_error(dev_msg: &str, app_mode: web::Data<String>) -> HttpResponse<BoxBody> {
+    let message = match app_mode.to_string().as_str() {
+        "dev" => dev_msg.to_string(),
+        _ => "".to_string(),
+    };
+
+    return HttpResponse::InternalServerError().body(message);
+}
